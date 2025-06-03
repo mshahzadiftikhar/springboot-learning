6e0a957568008cda72d1d7a5508dcf9970484505
mshahzadiftikhar <shahzadiftikhar@outlook.com>
2025-06-03 06:48:21 +0500

    [newStarterProject: added new starter project and updated readme file]

---
+++ org.learning.spring.boot.learning/src/test/java/org/learning/spring/boot/org/learning/spring/boot/learning/ApplicationTests.java
@@ -0,0 +1,13 @@
+package org.learning.spring.boot.org.learning.spring.boot.learning;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
