4e3ad76f45d14455351eaef93d499bf163e1e89a
mshahzadiftikhar <shahzadiftikhar@outlook.com>
2025-06-19 07:40:22 +0500

    [github-actions]: Added cicd file for github actions

--- org.learning.spring.boot.learning/src/test/java/org/learning/spring/boot/org/learning/spring/boot/learning/ApplicationTests.java
+++ org.learning.spring.boot.learning/src/test/java/org/learning/spring/boot/org/learning/spring/boot/learning/ApplicationTests.java
@@ -1,13 +1,16 @@
 package org.learning.spring.boot.org.learning.spring.boot.learning;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
 import org.junit.jupiter.api.Test;
+import org.learning.spring.boot.learning.Application;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
+@SpringBootTest(classes = Application.class)
 class ApplicationTests {
 
 	@Test
 	void contextLoads() {
+		assertEquals("1", "1");
 	}
-
 }
